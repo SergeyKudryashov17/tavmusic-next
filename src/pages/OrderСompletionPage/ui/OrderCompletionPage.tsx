8b6810befc04a9dce093bb098702c9d09d4883f9
Sergey Kudryashov <sergey.alex.kudryashov@yandex.ru>
2024-12-01 21:06:14 +0300

    Реализована страница подтверждения заказа + подключен tailwind + мелкие правки стилизации

--- src/pages/OrderСompletionPage/ui/OrderCompletionPage.tsx
+++ src/pages/OrderСompletionPage/ui/OrderCompletionPage.tsx
@@ -1,11 +1,38 @@
 import Container from "@/shared/ukit/Container";
 import Section from "@/shared/ukit/Section";
+import Headline, {HeadlineViews} from "@/shared/ukit/Headline";
+import Button from "@/shared/ukit/Button";
+import './OrderCompletionPage.scss';
+import {ReactNode} from "react";
+import Image from 'next/image';
+import SuccessImage from "@/shared/assets/img/ConfirmedOrder.png";
+import Link from "next/link";
 
-const OrderCompletionPage = () => {
+const OrderCompletionPage = (): ReactNode => {
   return (
     <Container>
       <Section>
-        <div>Оформление заказа завершено</div>
+        <div className="grid grid-cols-2 gap-1 items-center">
+          <div className="">
+            <div className="labelCompleteOrder">Оформление заказа завершено</div>
+            <Headline view={HeadlineViews.H1} style={{lineHeight: '1.3'}}>
+              Ваш заказ успешно оформлен!
+            </Headline>
+            <div className="textDetailOrder">
+              Вся информация по заказу отправлена вам на почту. Отследить статус заказа
+              можно по ссылке из письма.
+            </div>
+            <Link href="/">
+              <Button isPrimary={true} className="btnContinueOrder">Продолжить покупки</Button>
+            </Link>
+          </div>
+          <div>
+            <Image
+              src={SuccessImage}
+              alt="Заказ успешно оформлен"
+            />
+          </div>
+        </div>
       </Section>
     </Container>
   );
